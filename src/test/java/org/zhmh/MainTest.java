d0e96c514476a3324f2811d25f6b1a825b6176ed
zhmhbest <zhmhbest@gmail.com>
2021-03-12 01:26:56 +0800

    MultiTemplateString++

--- src/test/java/org/zhmh/MainTest.java
+++ src/test/java/org/zhmh/MainTest.java
@@ -61,4 +61,20 @@ public class MainTest {
         Assert.assertEquals(lines[3], "<form action=\"localhost/user/login\">");
         Assert.assertEquals(lines[8], "    <pre>${abc}</pre>");
     }
+
+    @Test
+    public void MultiTemplateString() {
+        MultiTemplateString mts = new MultiTemplateString(TypedString.getTemplate("^^^${a}|${b}|${c}$$$"));
+        mts.addSubSimpleTemplate("a", "[${aa}]");
+        Assert.assertEquals(mts.toString(),
+                "^^^[${Undefined variable 'aa'}]|${Undefined variable 'b'}|${Undefined variable 'c'}$$$");
+        mts.setVariable("aa", "AA");
+        mts.addSubMultiTemplate("b", "(${c})");
+        mts.addSubMultiTemplate("c", "[${d}]");
+        mts.addSubMultiTemplate("d", "{${d1}+${d2}+${d3}}");
+        mts.setVariable("d1", "11");
+        mts.setVariable("d2", "22");
+        mts.setVariable("d3", "33");
+        Assert.assertEquals(mts.toString(), "^^^[AA]|([{11+22+33}])|[{11+22+33}]$$$");
+    }
 }
